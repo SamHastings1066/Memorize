c97ac1aa13b21db4934bd8ded17292b5525fdd21
Sam Hastings <samhastingsis@gmail.com>
2023-10-27 11:00:13 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by sam hastings on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
